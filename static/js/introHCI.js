bbd68c2dd2aef7df3eb212bb30d676501418d567
Fiona Feng <ffeng@ucsd.edu>
2021-01-21 01:50:50 -0800

    completed

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -9,10 +9,24 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	$("a.thumbnail").click(projectClick);
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript: The One And Only");
+    $("#testjs").text("Please wait...");
+    $(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) { 
+    e.preventDefault();  
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(description).fadeToggle();
+    }
 }
